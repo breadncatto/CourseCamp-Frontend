fcb136e153d81d9c534c6a30f3ab016a2a9e638d
NewtakaNghia <nghia.trantrong0409@hcmut.edu.vn>
2026-04-10 22:12:17 +0700

    feat: Adding .env and replace API_URL

--- client/src/api/instructorService.js
+++ client/src/api/instructorService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/api";
+const API_URL = import.meta.env.VITE_BACKEND_URL;
 
 export const getMyCourses = async () => {
     const token = sessionStorage.getItem('token');
